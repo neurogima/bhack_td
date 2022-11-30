10eb60966fffc6179cb9108f6b48b5e9f2cc2653
Christelle Zielinski <chris.zielinski@free.fr>
2022-11-30 16:31:45 +0100

    debug task3 + parallel test

--- code/bhack_task3_distances_parallel.py
+++ code/bhack_task3_distances_parallel.py
@@ -2,15 +2,20 @@
 """
 Created on Mon Nov 28 10:40:57 2022
 
-@author: marieplgt,chris-zielinski
+@author: marieplgt, chris-zielinski
+
+some multiprocessing tests
+
 """
-	
-# In[set environment]:
+
 import scipy as sc
 from scipy import io
 import numpy as np
 import time
+import multiprocessing as mp
+import itertools
 
+# initial function with 2 nested for loop
 def compute_dist(dat, type='cosine'):
     """Return distance using scipy function
     Parameters
@@ -25,41 +30,43 @@ def compute_dist(dat, type='cosine'):
     """
     # get the data shape
     ndim = np.shape(dat)
-    # init the output
     n_pair = int(ndim[0]*(ndim[0]-1)/2)
     dist = np.zeros((n_pair, ndim[2], ndim[3]))
-    # loop // dim
     for i in range(ndim[2]):
         for j in range(ndim[3]):
             dist[:, i, j] = sc.spatial.distance.pdist(dat[:,:,i,j], metric=type)
     return dist
 
+def comp_distc(vec, params):
+    i = params[0]
+    j = params[1]
+    d = sc.spatial.distance.pdist(vec[:,:,i,j], metric='euclidean')
+    return d   
+
 if __name__ == '__main__':
-    # load the matlab data
+    # fname = os.path.join('.\results', 'bhack_task_02_output_temporalfolding.mat') 
     fname = 'C:\\home\oprojects\\brainhack22\\bhack_td\\results\\bhack_task_02_output_temporalfolding.mat'
     tmp = io.loadmat(fname)
 
-    Xs = tmp['Xs'][0] 
-    del tmp
-    # distance computation for the 3 data types
-
-    start_time = time.perf_counter()
-    # acoustic data # (489, 3, 90, 10)
-    # 90: predictor
-    # 10: timechunks
-    dist_ac = compute_dist(Xs[0], 'cosine')
-    print("Shape: acoustic= {}".format(np.shape(dist_ac)))
-    # sem data # (489, 1536, 18, 10)
-    dist_sem = compute_dist(Xs[1], 'cosine')
-    print("Shape: sem= {}".format(np.shape(dist_sem)))
-    # eeg data # (489, 3, 3, 10)
-    dist_eeg = compute_dist(Xs[2], 'euclidean')
-    print("Shape: eeg= {}".format(np.shape(dist_eeg)))
-    finish_time = time.perf_counter()
-    print("Computing time {} s".format(finish_time-start_time))
+    Xs = tmp['Xs'][0]
     
-    Ds = [[dist_ac], [dist_sem], [dist_eeg]]
-    # io.savemat('C:\\home\oprojects\\brainhack22\\test_sem.mat', {'sem': dist_sem})
+    start_time = time.perf_counter()
+    dist_eeg = compute_dist(Xs[2], 'euclidean')
+    finish_time = time.perf_counter()
+    print("Program finished in {} s".format(finish_time-start_time))
+    
+    start_time = time.perf_counter()
+    pool = mp.Pool(mp.cpu_count())
+    dat = Xs[2]
+    ndim = np.shape(dat)
+    comb = list(itertools.product(range(ndim[2]), range(ndim[3])))
+    results = [pool.apply(comp_distc, args=(dat, params)) for params in comb]
+    finish_time = time.perf_counter()
+    print("Program finished in {} s".format(finish_time-start_time))
+    pool.close() 
+    # Program finished in 0.5110705999977654 s
+    # Program finished in 5.315561700001126 s
+    # --> it failed...
 
 
 
